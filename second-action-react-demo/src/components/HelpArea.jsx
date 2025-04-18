4ba0f3f5313c1ca4114e49a67c176aade044edf8
Marco Almeida <marcoantonio.aa09@gmail.com>
2025-04-18 15:21:19 -0300

    trigging test.yml

--- second-action-react-demo/src/components/HelpArea.jsx
+++ second-action-react-demo/src/components/HelpArea.jsx
@@ -5,7 +5,7 @@ const HELP_ITEMS = [
   {
     id: 'h1',
     title: 'What is Git?',
-    text: 'Git is a version control system, helping you to manage your code and create code snapshots."Trigging test"',
+    text: 'Git is a version control system, helping you to manage your code and create code snapshots."Trigging test again"',
   },
   {
     id: 'h2',
